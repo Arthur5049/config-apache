e748488db2d9ce20ec75e6a4b944bd9542947bf5
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2022-01-12 21:06:46 -0300

    add commit

--- start-cluster.sh
+++ start-cluster.sh
@@ -5,7 +5,7 @@ sudo service mysql start
 start-all.sh
 start-master.sh -p ${SPARK_MASTER_PORT}
 alias start-worker="start-worker.sh ${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}"
-start-history-server.sh                                                                                        
+start-history-server.sh
 
 # if [[ -z $startCluster ]]; then
 #     echo "Please inform the source, case loss yarn application -kill _id"
