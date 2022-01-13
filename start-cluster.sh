d8a9576898e0c32e1ccd1e89c9eb91fd043bdf35
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2022-01-12 21:17:02 -0300

    asd

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,20 +1,20 @@
 # sudo rm -R /tmp/*
 sudo service mysql start
 
-#------------------ spark ------------------------------------
-start-all.sh
-start-master.sh -p ${SPARK_MASTER_PORT}
-alias start-worker="start-worker.sh ${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}"
-start-history-server.sh
-
 # if [[ -z $startCluster ]]; then
 #     echo "Please inform the source, case loss yarn application -kill _id"
 #     echo " -> startCluster"
 # else
 
+#spark-start
+start-all.sh
+start-master.sh -p ${SPARK_MASTER_PORT}
+start-worker.sh ${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
+start-history-server.sh                                                                             
+
 #hadoop-start
-stop-dfs.sh
-stop-yarn.sh
+# stop-dfs.sh
+# stop-yarn.sh
 start-dfs.sh
 start-yarn.sh
 jps
